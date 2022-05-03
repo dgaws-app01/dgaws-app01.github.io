2d41aa620aa2b07b33378c4f76eae3b35017a0b8
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2022-05-03 09:34:49 -0700

    Update app.js

--- stox/js/app.js
+++ stox/js/app.js
@@ -24,6 +24,33 @@ try {
             let mainDiv = d.createElement("div")
             mainDiv.innerHTML = "Stox App Started !"
             b.appendChild(mainDiv)
+            var f = fetch("https://www.indiainfoline.com/api/cmotapi.php?url=equity/market.svc/Sensex-Nifty-Ticker?responseType=json", {
+                headers: {
+                    accept: "application/json, text/javascript, */*; q=0.01",
+                    "accept-language": "en-US,en;q=0.9",
+                    "cache-control": "no-cache",
+                    pragma: "no-cache",
+                    "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"',
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": '"Windows"',
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-origin",
+                    "x-requested-with": "XMLHttpRequest",
+                },
+                referrer: "https://www.indiainfoline.com/",
+                referrerPolicy: "strict-origin-when-cross-origin",
+                body: null,
+                method: "GET",
+                mode: "cors",
+                credentials: "include",
+            })
+            f.then(d =>
+                d.json().then(j => {
+                    b.innerText = JSON.stringify(j)
+                })
+            )
+
             alert("main div loaded")
         },
     }
@@ -33,4 +60,3 @@ try {
 } catch (er) {
     console.log(er)
 }
-
