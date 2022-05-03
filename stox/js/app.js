2cff554cbc9aa1fe89c063f790593c0c360a91ad
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2022-05-03 07:57:40 -0700

    Update app.js

--- stox/js/app.js
+++ stox/js/app.js
@@ -1,2 +1,3 @@
+// let s = document.createElement("script"); s.src = "https://dgaws-app01.github.io/stox/js/app.js"; document.head.appendChild(s)
 alert(`${document.body.children.length} no of elements !`)
 document.body.innerHTML = ""
