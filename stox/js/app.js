78444c89f2a566d7fea31e5d0cb644b99c4dab8d
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2022-05-03 09:21:22 -0700

    Update app.js

--- stox/js/app.js
+++ stox/js/app.js
@@ -2,3 +2,28 @@
 
 alert(`${document.body.children.length} no of elements !`)
 document.body.innerHTML = ""
+
+var stoxApp = {
+    loadCSS: () => {
+        let css = `@import url("https://fonts.googleapis.com/css2?family=Oxygen:wght@300&family=Titillium+Web:ital,wght@0,200;0,600;0,700;0,900;1,200;1,600;1,700&display=swap");
+
+        * {
+            font-family: "Titillium Web", sans-serif;
+        }`
+        let lnk = document.createElement("link")
+        lnk.rel = "stylesheet"
+        lnk.href = URL.createObjectURL(new Blob([css]))
+        document.head.appendChild(lnk)
+    },
+    draw: () => {
+        let d = document
+        let b = d.body
+        let mainDiv = d.createElement("div")
+        mainDiv.innerHTML = "Stox App Started !"
+        b.appendChild(mainDiv)
+    },
+}
+
+stoxApp.loadCSS()
+stoxApp.draw()
+
