6603e15331c049711a2f3bd001ad9716c65a01fa
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2022-05-03 08:54:10 -0700

    Update app.js

--- stox/js/app.js
+++ stox/js/app.js
@@ -1,5 +1,5 @@
 // var s = document.createElement('script'); s.src = 'https%3A//dgaws-app01.github.io/stox/js/app.js'; document.head.appendChild(s);
-// alert("https%3A//dgaws-app01.github.io/stox/js/app.js")
+// alert("https%3A%2F%2Fdgaws-app01.github.io%2Fstox%2Fjs%2Fapp.js")
 // var%20s%20%3D%20document.createElement%28%27script%27%29%3B%20s.src%20%3D%20%27https%253A%2F%2Fdgaws-app01.github.io%2Fstox%2Fjs%2Fapp.js%27%3B%20document.head.appendChild%28s%29%3B
 alert(`${document.body.children.length} no of elements !`)
 document.body.innerHTML = ""
