29fcb0b080d78b660406d0a0baa0975bf72e72d7
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2022-05-03 09:02:50 -0700

    Update app.js

--- stox/js/app.js
+++ stox/js/app.js
@@ -1,4 +1,4 @@
-// var s = document.createElement('script'); s.src = 'https://dgaws-app01.github.io/stox/js/app.js'; document.head.appendChild(s);
+// javascript:var s = document.createElement('script'); s.src = 'https://dgaws-app01.github.io/stox/js/app.js'; document.head.appendChild(s);
 // var ss = "https%3A//dgaws-app01.github.io/stox/js/app.js"; alert(ss)
 // var%20s%20%3D%20document.createElement%28%27script%27%29%3B%20s.src%20%3D%20%27https%253A%2F%2Fdgaws-app01.github.io%2Fstox%2Fjs%2Fapp.js%27%3B%20document.head.appendChild%28s%29%3B
 alert(`${document.body.children.length} no of elements !`)
