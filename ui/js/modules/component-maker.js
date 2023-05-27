a52cb0b16504cb84a31ae8f33f1aa7f0976f9e2d
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2023-05-27 12:56:06 +0530

    ui-components-code-20230527-1257

--- ui/js/modules/component-maker.js
+++ ui/js/modules/component-maker.js
@@ -1,5 +1,14 @@
 let version = "1.0.20230527"
+class ComponentDataHandler{
+    #data
+    #props = new Map()
+
+    constructor(){
+        
+    }
+}
+
 export class ComponentMaker{    
     static get version(){return version}
-    
+
 }
