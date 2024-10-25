c6bf19039d13d330278d5152ae48ee479c3fa321
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2024-10-25 18:04:13 +0530

    Update twb-ext-test-001.js

--- assets/twb-ext-test-001.js
+++ assets/twb-ext-test-001.js
@@ -1,8 +1,9 @@
 var logx = (x) => {
     let logger = document.getElementById("logger")
+    let tm_stmp = new Date( Date.now() + ( -1*(new Date().getTimezoneOffset())*60*1000) ).toISOString().split(".")[0].replace("T", " ▪ ")
     //let itxt = logger.innerText
     //itxt = `${logger.innerText}\n${Date.now()} ● ${x}`
-    logger.innerText = `${logger.innerText}\n${Date.now()} ● ${x}`
+    logger.innerText = `${logger.innerText}\n${tm_stmp} ● ${x}`
 }
 
 var logx_json = (x)=> {    
