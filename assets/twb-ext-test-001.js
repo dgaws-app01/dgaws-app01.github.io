e42b1c7bebd7c7746ffc7fd507cf182f0cc3878f
dgaws-app01 <65401460+dgaws-app01@users.noreply.github.com>
2024-10-25 16:58:47 +0530

    Update twb-ext-test-001.js

--- assets/twb-ext-test-001.js
+++ assets/twb-ext-test-001.js
@@ -1,2 +1,14 @@
-alert("loaded")
-console.log("lion")
+var logx = (x) => {
+    let logger = document.getElementById("logger")
+    logger.innerText = x
+}
+
+var log_test_as_json = (x)=> {    
+    logx(JSON.stringify(x))
+  }
+
+var log_test_obj = (o)=> {
+    let op = []
+    Object.getOwnPropertyNames().forEach(p=> op.push(p))
+    logx(op)
+}
